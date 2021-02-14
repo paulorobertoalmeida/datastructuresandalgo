243066711d73271b0a077853be1422d5906528be
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-02-14 23:36:22 +0100

    Fixing the code

--- binarysearch.js
+++ binarysearch.js
@@ -1,4 +1,4 @@
-function binarySearch(arr elem){
+function binarySearch(arr, elem){
     var start = 0;
     var end = arr.length -1;
     var middle = Math.floor((start + end) / 2);
@@ -9,8 +9,10 @@ function binarySearch(arr elem){
         } else {
             start = middle +1;
         }
+        middle = Math.floor((start + end) / 2);
     }
-
+    console.log(start, middle, end);
+    return middle;
 }
 
 binarySearch([1,3,4,7,8,10,12,13,15,16,18], 7)
