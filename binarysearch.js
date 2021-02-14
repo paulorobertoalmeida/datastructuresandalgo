92cb42d0b23b81d58f527bbc5f735d533dcabe4a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-02-14 23:27:36 +0100

    Fist Commit Binary Search

---
+++ binarysearch.js
@@ -0,0 +1,16 @@
+function binarySearch(arr elem){
+    var start = 0;
+    var end = arr.length -1;
+    var middle = Math.floor((start + end) / 2);
+    console.log(start, middle, end);
+    while(arr[middle] !== elem){
+        if(elem < arr[middle]){
+            end = middle -1;
+        } else {
+            start = middle +1;
+        }
+    }
+
+}
+
+binarySearch([1,3,4,7,8,10,12,13,15,16,18], 7)
